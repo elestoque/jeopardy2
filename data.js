b580cf681defd40fcc495c8d269362addc5796e0
elestoque <mv.el.estoque@gmail.com>
2017-03-03 20:08:46 -0800

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,39 @@ window.GAME_STATE = {
   "Statistics": [
     {
       "amount": 100,
-      "clue": "soothing tool",
+      "clue": "What is the approximate number and percentage of Muslims in the U.S.?",
+      "answer": "3.5 million, around 1 percent of U.S. population",
+      "revealed": false
+    },
+    {
+      "amount": 200,
+      "clue": "What percentage of Muslim women in the U.S. actually wear hijabs?",
+      "answer": "43 percent of Muslim women wear hijabs",
+      "revealed": false
+    },
+    {
+      "amount": 300,
+      "clue": "List 5 of the 7 countries that Trump’s Muslim Ban covers.",
+      "answer": "Possible answers: Iran, Syria, Yemen, Iraq, Somalia, Sudan, and Libya",
+      "revealed": false
+    },
+    {
+      "amount": 400,
+      "clue": "Which country has the highest percentage of Muslims?",
+      "answer": "Indonesia has 87.2 percent Muslim population",
+      "revealed": false
+    },
+    {
+      "amount": 400,
+      "clue": "Which country has the highest percentage of Muslims?",
+      "answer": "Indonesia has 87.2 percent Muslim population",
+      "revealed": false
+    }
+  ],
+  "Famous Muslim": [
+    {
+      "amount": 100,
+      "clue": "Jack and Jill went up the hill to fetch __________",
       "answer": "What is a pacifier?",
       "revealed": false
     },
@@ -25,49 +57,23 @@ window.GAME_STATE = {
       "revealed": false
     }
   ],
-  "Statistics": [
+  "History": [
     {
       "amount": 100,
-      "clue": "soothing tool",
-      "answer": "What is a pacifier?",
+      "clue": "What is the name of one of the biggest muslim empires in history?",
+      "answer": "Ottoman Empire",
       "revealed": false
     },
     {
       "amount": 200,
-      "clue": "cleaning squares",
-      "answer": "What are baby wipes?",
+      "clue": "What was the primary method through which Islam spread to lands outside of Saudi Arabia?",
+      "answer": "Trade",
       "revealed": false
     },
     {
       "amount": 300,
-      "clue": "five point safety harness",
-      "answer": "What is a car seat?",
-      "revealed": false
-    },
-    {
-      "amount": 400,
-      "clue": "found 'in the tree' tops",
-      "answer": "What is a cradle?",
-      "revealed": false
-    }
-  ],
-  "Statistics": [
-    {
-      "amount": 100,
-      "clue": "soothing tool",
-      "answer": "What is a pacifier?",
-      "revealed": false
-    },
-    {
-      "amount": 200,
-      "clue": "cleaning squares",
-      "answer": "What are baby wipes?",
-      "revealed": false
-    },
-    {
-      "amount": 300,
-      "clue": "five point safety harness",
-      "answer": "What is a car seat?",
+      "clue": "In which year did the Prophet Muhammad (pbuh) first receive the Quran?",
+      "answer": "610 CE",
       "revealed": false
     },
     {
